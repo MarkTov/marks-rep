61c30f85cbd9661feee42e690a44b582f3efbdbe
MarkTov <149862141+MarkTov@users.noreply.github.com>
2024-02-14 07:20:10 +0000

    lesson and HW 6 commit

---
+++ HW#6.py
@@ -0,0 +1,17 @@
+a = 8*12*3;
+b = 8*8*6;
+c = 8*8*8;
+if a<b or a<c:
+    print("a<b or a<c min val", a);
+
+elif b<a or b<c:
+    print("b<a or b<c min val", b);
+
+
+elif c<a or c<b:
+    print("c<a or c<b min val", c);
+
+
+
+else:
+    ("a = b or b = c or a = c");
---
+++ lesson6.py
@@ -0,0 +1,11 @@
+a = 5*6*8;
+b = 3*4*12;
+
+if a>b:
+    print("a>b max val", a);
+
+elif b>a:
+    print("b>a max val", b);
+
+else:
+    ("a = b");
