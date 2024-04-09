88500a3175d272786cc9327221a2849b30515c66
MarkTov <149862141+MarkTov@users.noreply.github.com>
2024-04-09 06:00:03 +0000

    HW#12 commit

---
+++ lesson#12/num#14.py
@@ -0,0 +1,18 @@
+
+
+s = input("input 5 letter word for s ")
+s1 =s[0] +s[0] +s[1] +s[1] +s[2] +s[2] +s[3] +s[3] +s[4] +s[4]
+print(s1)
+
+w = input("input 5 letter word for w ")
+w1 = w[1] +w[0] +w[3] +w[2] +w[4]
+print(w1)
+
+
+h = input("input 5 letter word for h ")
+h1 = h[0] +h[4] +h[1] +h[4] +h[2] +h[4] +h[3] +h[4] +h[4] +h[4]
+print(h1)
+
+
+
+
